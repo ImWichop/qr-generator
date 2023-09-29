78be9c4aeba140e0ec77498164bf6e6035a38bec
Luis Carrillo <luis.carrilloo.perez@gmail.com>
2023-09-28 23:10:59 -0600

    [ADD] qr code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterOutlet } from '@angular/router'
+import { QRCodeModule } from 'angularx-qrcode'
+import { FormControl, ReactiveFormsModule } from '@angular/forms'
+import { SafeUrl } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, QRCodeModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'qr-generator'
+  textControl = new FormControl('')
+  qrCodeDownloadLink: SafeUrl = ''
+
+  onChangeURL(url: SafeUrl) {
+    this.qrCodeDownloadLink = url
+  }
 }
